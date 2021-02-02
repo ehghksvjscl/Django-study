9ccabfe94c1cdbe030cdbace4969fa26667a7d7d
김도환 <@DESKTOP-KN96EKC>
2021-02-02 23:47:08 +0900

    css with bootstrap

--- askcompany/instagram/urls.py
+++ askcompany/instagram/urls.py
@@ -18,7 +18,7 @@ app_name = 'instagram' # URL Reverse에서 namespace 역활을 하게 됩니다.
 
 urlpatterns = [
     path('',views.post_list, name='post_list'),
-    path('<int:pk>/',views.post_detail),
+    path('<int:pk>/',views.post_detail, name='post_detail'),
     # path('archives/<int:year>/',views.archives_year),
     # path(r'archives/(?P<year>20\d{2})/',views.archives_year), # 쿼리 파람값을 20~~ 로 받기
     # path('archives/<year:year>/',views.archives_year), # Converter 정의
