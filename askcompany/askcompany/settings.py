fb5222ea623f9bc9996fb168f5d13b324a9299d1
김도환 <@DESKTOP-KN96EKC>
2021-02-05 09:50:43 +0900

    mearge

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+print(os.path.join(BASE_DIR, 'static'))
 
 # Media
 MEDIA_URL = '/media/'
