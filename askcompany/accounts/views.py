a032c8cfac4589970844bde29ff5ac480223878f
김도환 <@DESKTOP-KN96EKC>
2021-02-09 17:54:24 +0900

    profile update page create

--- askcompany/accounts/views.py
+++ askcompany/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView
+from .models import Profile
+from .forms import ProfileForm
+# @login_required
+# def profile(request):
+#     return render(request,'accounts/profile.html') 
 
-# Create your views here.
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = "accounts/profile.html"
+
+profile = ProfileView.as_view()
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST,request.FILES,instance=profile)
+        if form.is_valid():
+            form.save(commit=False)
+            form.user = request.user
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request,'accounts/profile_form.html',{'form':form})
+
+# class ProfileUpdateView(LoginRequiredMixin,UpdateView):
+#     model = profile
+#     form_class = ProfileForm
+
+# profile_edit = ProfileUpdateView.as_view()
+
