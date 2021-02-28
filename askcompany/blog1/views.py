facb5c145cc03404fe531e3cb3ef51c3de88b8e7
Kim Do Hwan <dhkim3@aspringcloud.com>
2021-03-01 06:43:59 +0900

    SPA Django Full Stack Start Project

--- askcompany/blog1/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-def post_list(request):
-    qs = models.Post.objects.all()
-    content = {'post_list' : qs}
-    return render(request,'blog1/post_list.html',content)
