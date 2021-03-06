ab77aa17b00a919de9a80b132bef0b1afc5041b4
김도환 <@DESKTOP-KN96EKC>
2021-03-07 03:52:03 +0900

    base file & gitignore setting

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static file path setting
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-
+    os.path.join(BASE_DIR,'askcompany','static') # template setting
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
