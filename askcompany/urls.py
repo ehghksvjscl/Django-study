29005985f57e92ff1c5f69f083fbe3c6e487e81e
Kim Do Hwan <dhkim3@aspringcloud.com>
2021-03-10 16:17:48 +0900

    django-pydenticon을 활용하여 프로필 디폴트 이미지 구현

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -5,10 +5,17 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+# icon create package
+# ref = https://django-pydenticon.readthedocs.io/en/0.2/installation.html
+# ref = https://github.com/azaghal/django-pydenticon/blob/master/django_pydenticon/urls.py
+# import django_pydenticon.urls -> django 2버전에서 만들어 진거라 django-pydenticon views 참조 후 변경
+from django_pydenticon.views import image as pydenticon_image 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
-    path('',login_required(TemplateView.as_view(template_name='root.html')),name='root')
+    path('',login_required(TemplateView.as_view(template_name='root.html')),name='root'),
+    path('identicon/image/<path:data>/', pydenticon_image,name='pydenticon_image'),
 ]
 
 if settings.DEBUG:
